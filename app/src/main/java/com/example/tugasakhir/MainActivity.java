927c856cbf3d8def2238fde757a8ad439bf5a405
metatoda42 <72773349+metatoda42@users.noreply.github.com>
2021-06-14 10:11:08 +0700

    Tes commit

--- app/src/main/java/com/example/tugasakhir/MainActivity.java
+++ app/src/main/java/com/example/tugasakhir/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.tugasakhir;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//Testing, testing 123
 public class MainActivity extends AppCompatActivity {
 
     @Override

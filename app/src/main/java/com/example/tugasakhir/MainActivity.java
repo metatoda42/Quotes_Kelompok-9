89dfeef62eb89f07056bac759b2b353f85767f25
metatoda42 <72773349+metatoda42@users.noreply.github.com>
2021-06-16 19:23:35 +0700

    Tampilan Skuyyy

--- app/src/main/java/com/example/tugasakhir/MainActivity.java
+++ app/src/main/java/com/example/tugasakhir/MainActivity.java
@@ -23,53 +23,12 @@ import java.util.List;
 //Tes Lagi
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
     private BottomNavigationView navbar;
-    //Deklarasi Variabel, kayak di C++ gitu
-    //Bedanya, disini variabelnya adalah komponen komponen XML yang nantinya akan diubah.
-    //Contoh, kalo biasanya situ belajar tentang variabel x bisa diisi dengan angka atau text
-    //Di Android Studio, variabel editText dideklarasi sebagai EditText sehingga bisa menggantikan
-    // komponen EditText yang ada di dalam XML.
 
-    //Selanjutnya, ada Variabel btnAdd. Sifatnya adalah Button, jadi variabel ini bisa diganti
-    //fungsinya ketika di klik.
-
-    //Recycler View itu intinya, suatu view secara keseluruhan. Jadi seluruh konten tertentu di
-    //masukan kedalam satu variabel besar bernama recyclerView.
-
-    EditText editText; //Variabel EditText
-    ImageButton btAdd, btReset; //Variabel Button
-    RecyclerView recyclerView; //Variabel RecyclerView
-
-    //Nah, ini juga variabel. Varibel ini adalah variabel "dataList" dengan jenis data List.
-    //Dalam kasus ini, data yang terdapat dalam list ini di handle oleh kelas MainData.
-    List<MainData> dataList = new ArrayList<>();
-    LinearLayoutManager linearLayoutManager;
-    DBKomen database;
-    AdapterChan adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Dibagian ini, variabel ini dihubungkan dengan komponen yang berada di XML
-
-        //Nanti tapi, aku mau ngurus adapter bentar...
-        //JIANCUK SUSAH BANGET ANJINGGGGG!!!!
-
-
-
-
-
-
-
-        //-------------------------------------
-
-
-
-
-
-
-        //Nah, kalo ini kodingan yang akan mengurus tampilan
 
         navbar = findViewById(R.id.main_navbar);
         navbar.setOnNavigationItemSelectedListener(this);
